ea735ff9b43998024e07062a47b56926e1317d2a
fscarmen2 <fs.carmen@gmail.com>
2023-06-16 14:40:06 +0000

    Argo + Xray for botshard.

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# X-for-Botshard-ARM
+# Argo Xray for Botshard (ARM)
+
+* * *
+
+# 目录
+
+- [项目特点](README.md#项目特点)
+- [部署](README.md#部署)
+
+* * *
+
+## 项目特点:
+* 本项目用于在 [Botshard.com](https://dashboard.botshard.com/) 免费服务上部署 Xray
+* 根据本平台自行修改的哪吒探针，以能使用其 VNC 达到 ssh 的效果，可以自由选择是否安装
+* 使用 CloudFlare 的 Argo 隧道，直接优选 + 隧道，CDN 不用再做 workers
+* 针对本项目另辟蹊径处理 root 权限，可玩性大大增加
+* 部署完成如发现不能上网，请检查域名是否被墙，可使用 Cloudflare CDN 或者 worker 解决。参照文章：https://www.hicairo.com/post/57.html
+* 前端 js 定时保活，会玩的用户可以根据具体情况修改间隔时间
+* 节点信息以 V2rayN / Clash / 小火箭 链接方式输出
+* 可以使用浏览器使用 webssh 和 webftp，更方便管理系统
+
+## 部署:
+* 注册 [Botshard.com](https://panel.botshard.com/)
+* entrypoint.sh 的 6-8 行修改 UUID、WS 路径和哪吒变量
+* PaaS 平台用到的变量
+
+  | 变量名        | 是否必须 | 默认值 | 备注 |
+  | ------------ | ------ | ------ | ------ |
+  | UUID         | 否 | de04add9-5c68-8bab-950c-08cd5320df18 | 可在线生成 https://www.zxgj.cn/g/uuid |
+  | WSPATH       | 否 | argo | 勿以 / 开头，各协议路径为 `/WSPATH-协议`，如 `/argo-vless`,`/argo-vmess`,`/argo-trojan`,`/argo-shadowsocks` |
+  | NEZHA_SERVER | 否 |        | 哪吒探针与面板服务端数据通信的IP或域名 |
+  | NEZHA_PORT   | 否 |        | 哪吒探针服务端的端口 |
+  | NEZHA_KEY    | 否 |        | 哪吒探针客户端专用 Key |
+  | NEZHA_TLS    | 否 |        | 哪吒探针是否启用 SSL/TLS 加密 ，如不启用不要该变量，如要启用填"1" |
+  | ARGO_AUTH    | 否 |        | Argo 的 Token 或者 json 值 |
+  | ARGO_DOMAIN  | 否 |        | Argo 的域名，须与 ARGO_DOMAIN 必需一起填了才能生效 |
+  | WEB_USERNAME | 否 | admin  | 网页和 webssh 的用户名 |
+  | WEB_PASSWORD | 否 | password | 网页和 webssh 的密码 |
+  | SSH_DOMAIN   | 否 |        | webssh 的域名，用户名和密码就是 <WEB_USERNAME> 和 <WEB_PASSWORD> |
+  | FTP_DOMAIN   | 否 |        | webftp 的域名，用户名和密码就是 <WEB_USERNAME> 和 <WEB_PASSWORD> |
+
+* 需要应用的 js
+  | 命令 | 说明 |
+  | ---- |------ |
+  | <URL>/list | 查看节点数据 |
+  | <URL>/listen | 查看后台监听端口 |
+  | <URL>/test |	测试是否为只读系统 |
---
+++ index.js
@@ -0,0 +1,306 @@
+const server = process.env.SERVER_IP;
+const port = process.env.SERVER_PORT || 3000; /* 当容器平台分配不规则端口时,此处需修改为分配端口 */
+const express = require("express");
+const app = express();
+var exec = require("child_process").exec;
+const os = require("os");
+const { legacyCreateProxyMiddleware } = require("http-proxy-middleware");
+var request = require("request");
+var fs = require("fs");
+var path = require("path");
+const auth = require("basic-auth");
+
+// 获取 entrypoint.sh 文件里的相关 WEB_USERNAME 和 WEB_PASSWORD 值，默认值为 admin / password
+const entryPointPath = './entrypoint.sh';
+
+let username = 'admin';
+let password = 'password';
+
+for (const line of fs.readFileSync(entryPointPath, 'utf8').split('\n')) {
+  if (line.startsWith('WEB_USERNAME=')) {
+    username = line.split('=')[1];
+    break; // 找到第一个 WEB_USERNAME 的值后，立即停止遍历
+  }
+}
+
+for (const line of fs.readFileSync(entryPointPath, 'utf8').split('\n')) {
+  if (line.startsWith('WEB_PASSWORD=')) {
+    password = line.split('=')[1];
+    break; // 找到第一个 WEB_PASSWORD 的值后，立即停止遍历
+  }
+}
+
+console.log(`Username is: ${username}`);
+console.log(`Password is: ${password}`);
+
+// 首页显示内容
+app.get("/", function (req, res) {
+  res.send("hello world");
+});
+
+// 页面访问密码
+app.use((req, res, next) => {
+  const user = auth(req);
+  if (user && user.name === username && user.pass === password) {
+    return next();
+  }
+  res.set("WWW-Authenticate", 'Basic realm="Node"');
+  return res.status(401).send();
+});
+
+/* 系统没有 ps 指令
+//获取系统进程表
+app.get("/status", function (req, res) {
+  let cmdStr = "ps -ef";
+  exec(cmdStr, function (err, stdout, stderr) {
+    if (err) {
+      res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
+    }
+    else {
+      res.type("html").send("<pre>获取系统进程表：\n" + stdout + "</pre>");
+    }
+  });
+});
+*/
+
+//获取系统监听端口
+app.get("/listen", function (req, res) {
+    let cmdStr = "ss -nltp";
+    exec(cmdStr, function (err, stdout, stderr) {
+      if (err) {
+        res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
+      }
+      else {
+        res.type("html").send("<pre>获取系统监听端口：\n" + stdout + "</pre>");
+      }
+    });
+  });
+
+
+//获取节点数据
+app.get("/list", function (req, res) {
+    let cmdStr = "cat list";
+    exec(cmdStr, function (err, stdout, stderr) {
+      if (err) {
+        res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
+      }
+      else {
+        res.type("html").send("<pre>节点数据：\n\n" + stdout + "</pre>");
+      }
+    });
+  });
+
+//获取系统版本、内存信息
+app.get("/info", function (req, res) {
+  let cmdStr = "cat /etc/*release | grep -E ^NAME";
+  exec(cmdStr, function (err, stdout, stderr) {
+    if (err) {
+      res.send("命令行执行错误：" + err);
+    }
+    else {
+      res.send(
+        "命令行执行结果：\n" +
+          "Linux System:" +
+          stdout +
+          "\nRAM:" +
+          os.totalmem() / 1000 / 1000 +
+          "MB"
+      );
+    }
+  });
+});
+
+//文件系统只读测试
+app.get("/test", function (req, res) {
+  let cmdStr = 'mount | grep " / " | grep "(ro," >/dev/null';
+  exec(cmdStr, function (error, stdout, stderr) {
+    if (error !== null) {
+      res.send("系统权限为---非只读");
+    } else {
+      res.send("系统权限为---只读");
+    }
+  });
+});
+
+/*
+//启动root
+app.get("/root", function (req, res) {
+  let cmdStr = "bash root.sh >/dev/null 2>&1 &";
+  exec(cmdStr, function (err, stdout, stderr) {
+    if (err) {
+      res.send("root权限部署错误：" + err);
+    }
+    else {
+      res.send("root权限执行结果：" + "启动成功!");
+    }
+  });
+});
+*/
+
+// keepalive begin
+//web保活
+function keep_web_alive() {
+  // 1.请求主页，保持唤醒
+  request("http://[" + server + "]:" + port, function (error, response, body) {
+    if (!error) {
+      console.log("保活-请求主页-命令行执行成功，响应报文:" + body);
+    }
+    else {
+      console.log("保活-请求主页-命令行执行错误: " + error);
+    }
+  });
+
+  // 2.请求服务器进程状态列表，若web没在运行，则调起
+  exec("ss -nltp", function (err, stdout, stderr) {
+    // 1.查后台系统进程，保持唤醒
+    if (stdout.includes("web.js")) {
+      console.log("web 正在运行");
+    }
+    else {
+      //web 未运行，命令行调起
+      exec(
+        "chmod +x web.js && ./web.js -c ./config.json >/dev/null 2>&1 &", function (err, stdout, stderr) {
+          if (err) {
+            console.log("保活-调起web-命令行执行错误:" + err);
+          }
+          else {
+            console.log("保活-调起web-命令行执行成功!");
+          }
+        }
+      );
+    }
+  });
+}
+setInterval(keep_web_alive, 10 * 1000);
+
+//Argo保活
+function keep_argo_alive() {
+  exec("ss -nltp", function (err, stdout, stderr) {
+    // 1.查后台系统进程，保持唤醒
+    if (stdout.includes("cloudflared")) {
+      console.log("Argo 正在运行");
+    }
+    else {
+      //Argo 未运行，命令行调起
+      exec(
+        "bash argo.sh 2>&1 &", function (err, stdout, stderr) {
+          if (err) {
+            console.log("保活-调起Argo-命令行执行错误:" + err);
+          }
+          else {
+            console.log("保活-调起Argo-命令行执行成功!");
+          }
+        }
+      );
+    }
+  });
+}
+setInterval(keep_argo_alive, 30 * 1000);
+
+//哪吒保活
+function keep_nezha_alive() {
+  exec("pidof nezha-agent", function (err, stdout, stderr) {
+    // 1.查后台系统进程，保持唤醒
+    if (stdout != "" ) {
+      console.log("哪吒正在运行");
+    }
+    else {
+      //哪吒未运行，命令行调起
+      exec(
+        "bash nezha.sh 2>&1 &", function (err, stdout, stderr) {
+          if (err) {
+            console.log("保活-调起哪吒-命令行执行错误:" + err);
+          }
+          else {
+            console.log("保活-调起哪吒-命令行执行成功!");
+          }
+        }
+      );
+    }
+  });
+}
+setInterval(keep_nezha_alive, 45 * 1000);
+// keepalive end
+
+//下载web可执行文件
+app.get("/download", function (req, res) {
+  download_web((err) => {
+    if (err) {
+      res.send("下载文件失败");
+    }
+    else {
+      res.send("下载文件成功");
+    }
+  });
+});
+
+
+app.use( /* 具体配置项迁移参见 https://github.com/chimurai/http-proxy-middleware/blob/master/MIGRATION.md */
+  legacyCreateProxyMiddleware({
+    target: 'http://127.0.0.1:8080/', /* 需要跨域处理的请求地址 */
+    ws: true, /* 是否代理websocket */
+    changeOrigin: true, /* 是否需要改变原始主机头为目标URL,默认false */ 
+    on: {  /* http代理事件集 */ 
+      proxyRes: function proxyRes(proxyRes, req, res) { /* 处理代理请求 */
+        // console.log('RAW Response from the target', JSON.stringify(proxyRes.headers, true, 2)); //for debug
+        // console.log(req) //for debug
+        // console.log(res) //for debug
+      },
+      proxyReq: function proxyReq(proxyReq, req, res) { /* 处理代理响应 */
+        // console.log(proxyReq); //for debug
+        // console.log(req) //for debug
+        // console.log(res) //for debug
+      },
+      error: function error(err, req, res) { /* 处理异常  */
+        console.warn('websocket error.', err);
+      }
+    },
+    pathRewrite: {
+      '^/': '/', /* 去除请求中的斜线号  */
+    },
+    // logger: console /* 是否打开log日志  */
+  })
+);
+
+//初始化，下载web
+function download_web(callback) {
+  // 检查文件是否存在
+  if (fs.existsSync("./web.js")) {
+    callback(null);
+    return;
+  }
+
+  let web_url =
+    "https://github.com/fscarmen2/Argo-X-Container-PaaS/raw/main/files/web.js";
+  let stream = fs.createWriteStream(filePath);
+  request(web_url)
+    .pipe(stream)
+    .on("close", function (err) {
+      if (err) {
+        callback("下载文件失败");
+      }
+      else {
+        callback(null);
+      }
+    });
+}
+
+download_web((err) => {
+  if (err) {
+    console.log("初始化-下载web文件失败");
+  }
+  else {
+    console.log("初始化-下载web文件成功");
+  }
+});
+
+//启动核心脚本运行web,哪吒和argo
+exec("bash entrypoint.sh", function (err, stdout, stderr) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(stdout);
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
